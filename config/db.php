17f7a856ebbeccada262bd2721ceda83e0dfc1eb
Jerry Wong <jerry.wong@iproperty.com>
2019-01-16 18:00:49 +0800

    update

--- config/db.php
+++ config/db.php
@@ -4,6 +4,11 @@ $dbName = "sq_stationery";
 $userName = "root";
 $password = "root";
 
+$hostName = "localhost";
+$dbName = "sq_stationery";
+$userName = "sq_stationery";
+$password = "Hkjh17&4";
+
 $link = mysqli_connect($hostName, $userName, $password, $dbName);
 mysqli_query($link, "SET NAMES 'utf8'");
 if (mysqli_connect_errno())
